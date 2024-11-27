4d733a951cecdac759ff058f288f863b95504843
eric <2589325627@qq.com>
2024-11-27 13:31:06 +0800

    Update ScrumtoolApplicationTests.java
    更新测试用例

--- scrumTool/src/test/java/com/example/scrumtool/ScrumtoolApplicationTests.java
+++ scrumTool/src/test/java/com/example/scrumtool/ScrumtoolApplicationTests.java
@@ -1,13 +1,52 @@
 package com.example.scrumtool;
 
+import com.example.scrumtool.controller.Controller;
+import com.example.scrumtool.dao.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ScrumtoolApplicationTests {
 
+	@Autowired
+	Controller controller;
+	
 	@Test
-	void contextLoads() {
+	void testUpdateGroupInfo() {
+		try {
+			controller.updateGroupInfo("test", 12);
+		} catch (Exception e) {
+			//
+		}
+	}
+	
+	@Test
+	void testRegister() {
+		try {
+			User user = new User();
+			user.setEmail("123@qq.com");
+			user.setName("admin");
+			user.setPassword("123");
+			user.setRole("leader");
+			controller.register(user);
+		} catch (Exception e) {
+			//
+		}
+	}
+	
+	@Test
+	void testLogin() {
+		try {
+			User user = new User();
+			user.setEmail("123@qq.com");
+			user.setName("admin");
+			user.setPassword("123");
+			user.setRole("leader");
+			controller.login(user);
+		} catch (Exception e) {
+			//
+		}
 	}
 
 }
